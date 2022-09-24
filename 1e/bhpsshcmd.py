9285bc9cc2eee7bccd04fc229877f57b7175fc39
Michael DeRuiter <michaelalanderuiter@protonmail.com>
2022-09-24 12:01:37 -0500

    1e/: Moved programs to 1st edition folder

--- 1e/bhpsshcmd.py
+++ 1e/bhpsshcmd.py
@@ -7,15 +7,15 @@ import subprocess
 def ssh_command(ip, user, passwd, command):
   client = paramiko.SSHClient()
   client.set_missing_host_key_policy(
-    paramiko.AutoAddPolicy())
+      paramiko.AutoAddPolicy())
   client.connect(ip, 
-    username=user, password=passwd)
+      username=user, password=passwd)
   ssh_session =
-    client.get_transport().open_session()
+      client.get_transport().open_session()
   if ssh_session.active:
     ssh_session.exec_command(command)
     print ssh_session.recv(1024)
- return
+  return
  
 ssh_command('192.168.100.131',
             'justin', 'lovespython', 'id')
