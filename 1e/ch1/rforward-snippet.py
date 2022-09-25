e592bd4b1da131b87feaaff1d5dde3a43e69a0d9
Michael DeRuiter <michaelalanderuiter@protonmail.com>
2022-09-25 09:35:08 -0500

    rforward-snippet.py: First draft

--- 1e/ch1/rforward-snippet.py
+++ 1e/ch1/rforward-snippet.py
@@ -1,8 +1,16 @@
 #!/bin/env python2
 
+import socket
+import getpass
+import paramiko
+
 def main():
+  # Double-check that all necessary arguments
+  # are passed to the script before setting up
+  # the client connection
   options, server, remote = parse_options()
   password = None
+  
   if options.readpass:
     password = getpass.getpass("Enter SSH " +
                                "password: ")
@@ -36,3 +44,44 @@ def main():
     print('C-c: Port forwarding stopped.')
     sys.exit(0)
 
+def reverse_forward_tunnel(
+    server_port, remote_host, remote_port,
+    transport):
+  transport.request_port_forward('', server_port)
+  while True:
+    chan = transport.accept(1000)
+    if chan is None:
+      continue
+    thr = threading.Thread(target=handler,
+          args=(chan, remote_host, remote_port))
+    thr.setDaemon(True)
+    thr.start()
+
+def handler(chan, host, port):
+  sock = socket.socket()
+  
+  try:
+    sock.connect((host,port))
+  except Exception as e:
+    verbose('Forwarding request to ' +
+            '%s:%d failed: %r' % (host, port, e))
+    return
+  
+  verbose('Connected! Tunnel open ' +
+          '%r -> %r -> %r' % (chan.origin_addr,
+          chan.getpeername(), (host, port)))
+  
+  while True:
+    r,w,x = select.select([sock,chan],[],[])
+    if sock in r:
+      data = sock.recv(1024)
+      if len(data) == 0:
+        break
+      chan.send(data)
+    if chan in r:
+      data = chan.recv(1024)
+      if len(data) == 0:
+        break
+      sock.send(data)
+  
+  chan.close()
