0e9aceb0fa1d20528a2b183388e93d91dd335ce9
Jonathan Jay Eigelweisner <michaelalanderuiter@protonmail.com>
2022-12-01 09:21:53 -0600

    tcpserver.py: Initial draft, modified companion tcpclient.py

--- 2e/ch1/tcpclient.py
+++ 2e/ch1/tcpclient.py
@@ -2,8 +2,8 @@
 
 import socket
 
-target_host = "www.google.com"
-target_port = 80
+target_host = "0.0.0.0"
+target_port = 9998
 
 # Create a socket object
 client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
